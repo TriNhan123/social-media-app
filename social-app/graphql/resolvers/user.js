edc556f504a1adead20d859e52b42d4ac20e1e1c
TriNhan123 <trinhan99999@gmail.com>
2021-08-13 02:09:07 -0700

    register-new-user-to-db

--- social-app/graphql/resolvers/user.js
+++ social-app/graphql/resolvers/user.js
@@ -1,11 +1,44 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const { SECRET_KEY} = require('../../config');
 const User = require('../../models/User');
 
 module.exports = {
     Mutation: {
-        register(_, args, context, info){
+        async register(
+            _, 
+            {
+            registerInput: {username, email, password, comfirmPassword}
+            }, 
+            context, 
+            info
+            ){
             // TODO: Validate user data 
             // TODO: Make sure user hasn't already exist
             // TODO: hash password and create an auth token 
+            password = await bcrypt.hash(password, 12)
+            const newUser = new User({
+                email,
+                username, 
+                password, 
+                createdAt: new Date().toISOString()
+            });
+            
+            //save new registered user to db 
+            const res = await newUser.save();
+
+            const token = jwt.sign({
+                id: res.id,
+                email: res.email,
+                username: res.username
+            }, SECRET_KEY, { expiresIn: '1h'});
+
+            return {
+                ...res._doc,
+                id: res._id,
+                token 
+            };
         }
     }
 }
