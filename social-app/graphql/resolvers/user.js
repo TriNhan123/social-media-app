54c91c560040b6448171c6bd0d1a699bd096dc1d
TriNhan123 <trinhan99999@gmail.com>
2021-08-13 03:41:30 -0700

    validate-register-input

--- social-app/graphql/resolvers/user.js
+++ social-app/graphql/resolvers/user.js
@@ -1,22 +1,41 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { UserInputError } = require('apollo-server');
 
 const { SECRET_KEY} = require('../../config');
 const User = require('../../models/User');
+const { validateRegisterInput } = require('../../util/validators');
 
 module.exports = {
     Mutation: {
         async register(
             _, 
             {
-            registerInput: {username, email, password, comfirmPassword}
-            }, 
-            context, 
-            info
+            registerInput: {username, email, password, comfirmPassword }
+            }
             ){
-            // TODO: Validate user data 
-            // TODO: Make sure user hasn't already exist
-            // TODO: hash password and create an auth token 
+            // Validate user data 
+            const { valid, errors } = validateRegisterInput(
+                username, 
+                email, 
+                password, 
+                comfirmPassword
+            );
+            if(!valid){
+                throw new UserInputError('Errors', { errors });
+            }
+
+            //Make sure user hasn't already exist
+            const user = User.findOne({ username });
+            if(user){
+                throw new UserInputError('Username is taken', {
+                    errors: {
+                        username: 'This username is taken'
+                    }
+                })
+            }
+                
+            // hash password and create an auth token 
             password = await bcrypt.hash(password, 12)
             const newUser = new User({
                 email,
@@ -41,4 +60,4 @@ module.exports = {
             };
         }
     }
-}
+};
